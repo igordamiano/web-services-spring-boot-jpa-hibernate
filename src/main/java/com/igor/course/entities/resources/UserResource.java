b4a50f210df349f73b0b77d4c93496d88254f353
Igor <igordamiano@gmail.com>
2020-04-02 14:40:35 -0300

    User

---
+++ src/main/java/com/igor/course/entities/resources/UserResource.java
@@ -0,0 +1,30 @@
+package com.igor.course.entities.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.igor.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+
+		
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999-9999", "sua senha");
+		User u1 = new User(2L, "João", "joao@gmail.com", "8888-8888", "senha123");
+		
+		List<User> list = Arrays.asList(u, u1);
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+}
